73151efd1a1d016373ec4ba4d23386dec4604ce9
Nhung Nguyen <nhungnguyentester@gmail.com>
2022-12-13 23:31:42 +0700

    push code

---
+++ src/Lab_2/Bai1.java
@@ -0,0 +1,18 @@
+package Lab_2;
+
+import java.util.Scanner;
+
+public class Bai1 {
+  public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.println( "Please input your weight:");
+    float weight = input.nextFloat();
+    System.out.println("Please input your height: ");
+    float height =input.nextFloat();
+    input.close();
+
+    float BMI = weight / (height*2);
+    System.out.println("Your BMI is" + BMI);
+
+  }
+}
--- src/Lab_2/Bai3.java
+++
@@ -1,5 +0,0 @@
-package Lab_2;
-
-public class Bai3 {
-  //hello
-}
